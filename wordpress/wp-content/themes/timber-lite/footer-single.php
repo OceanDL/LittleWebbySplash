1bb11822fb2f0820fff3e47097966c17f7a16710
OceanDL <oceandouglas.lee@calbaptist.edu>
2019-08-03 10:33:58 -0700

    Wordpress config done

---
+++ wordpress/wp-content/themes/timber-lite/footer-single.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying the footer for single posts.
+ *
+ * @package Timber
+ * @since Timber 1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ){
+	exit; // Exit if accessed directly
+} ?>
+<footer id="colophon" class="site-footer  site-footer--single">
+	<?php timber_footer_the_copyright() ?>
+</footer><!-- #colophon -->
---
+++ wordpress/wp-content/themes/timber-lite/inc/integrations.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Require files that deal with various plugin integrations.
+*
+* @package Timber
+*/
+
+/**
+ * Load Jetpack compatibility file.
+ */
+require get_template_directory() . '/inc/integrations/jetpack.php';
